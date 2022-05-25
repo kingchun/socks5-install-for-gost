489290a228c335161cfb9643bc88cb878736fee2
春末丶冬初 <48005341+kingchun@users.noreply.github.com>
2022-05-25 20:21:21 +0800

    Update socks5_install.sh

--- socks5_install.sh
+++ socks5_install.sh
@@ -12,7 +12,7 @@ apt update &&yum update
 apt install wget curl gzip -y ||yum install wget curl gunzip -y
 
 wget https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
-gzip gost-linux-amd64-2.11.1.gz
+gunzip gost-linux-amd64-2.11.1.gz
 mv gost-linux-amd64-2.11.1.gz /usr/local/bin/gost
 chmod +x /usr/local/bin/gost
 
